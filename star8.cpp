0e2faed065872aa3073d6e085bad0b1ce82351e0
Abhishekjha2021 <abhishekj178btechcse2025@kccitm.edu.in>
2022-11-11 08:26:31 -0700

    new commits

--- star8.cpp
+++ star8.cpp
@@ -8,8 +8,13 @@ int main() {
     cin>>n;
     
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n+1-i;j++){
-            cout<<j<<" ";
+        for(int j=1;j<=i;j++){
+           if((i+j)%2==0){
+            cout<<"1"<<" ";
+           }
+           else{
+            cout<<"0"<<" ";
+           }
         }cout<<endl;
     }
     return 0;
