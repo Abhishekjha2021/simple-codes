b883c751bef71f698cba290e95f0f6601559d1ef
Abhishekjha2021 <abhishekj178btechcse2025@kccitm.edu.in>
2022-11-24 08:15:32 -0700

    new commits

--- temp21.cpp
+++ temp21.cpp
@@ -1,32 +1,16 @@
-// Online C++ compiler to run C++ program online
-#include <iostream>
-int binarysearch(int arr[],int n,int key);
+#include<iostream>
+#include<math.h>
+int sum(int n,int p);
 using namespace std;
-
-int main() {
-    int key;
-    int arr[9]={4,9,25,32,41,45,46,50,55};
-    int n=9;
-    cout<<"Enter a key to search in array"<<endl;
-    cin>>key;
-    cout<<binarysearch(arr,n, key);
+int main(){
+    cout<<sum(5,4);
     return 0;
 }
-int binarysearch(int arr[],int n,int key){
-    int s=0;
-    int e=n;
+int sum(int n,int p){
     
-    while(s<=e){
-        int mid=(s+e)/2;
-        
-        if(arr[mid]==key){
-            return mid;
-        }
-        else if(arr[mid]<key){
-            s=mid+1;
-        }
-        else{
-            e=mid-1;
-        }
+    if(p==0){
+        return 1;
     }
+    int prevfact=pow(n,p-1);
+    return n*prevfact;
 }
