433d93f6b0ebbbd842567e24dfd5c24bf6429443
Abhishekjha2021 <abhishekj178btechcse2025@kccitm.edu.in>
2022-12-07 00:18:38 -0700

    new commits

---
+++ recursion2.cpp
@@ -0,0 +1,17 @@
+//Recursion code to print numbers in decreasing order..
+#include<iostream>
+using namespace std;
+void func(int n);
+int main(){
+    func(5);
+    return 0;
+}
+
+void func(int n){
+    if(n==0){
+        return;
+    }
+    
+    cout<<n;
+    func(n-1);
+}
--- temp.cpp
+++ temp.cpp
@@ -1,29 +1,24 @@
 #include<iostream>
-
+int sortedarray(int arr[],int n);
 using namespace std;
-int sortedarrsy(int arr[],int n);
-
 int main(){
     int arr[5]={1,2,3,4,5};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    if (sortedarrsy(arr, n))
-        cout << "Yes\n";
-    else                                                       
-        cout << "No\n";
-    //cout<<sortedarrsy(arr[n], n           );
-    
-    return 0;                                                 
-}                                   
+    //cout<<sortedarray(arr,5);
+    if(sortedarray(arr,5)){
+        cout<<"yes";
+    }
+    else{
+        cout<<"no";
+    }
 
-int sortedarrsy(int arr[],int n){
+    return 0;
+}
+int sortedarray(int arr[],int n){
     if(n==0 || n==1){
         return 1;
     }
-    
-    if(arr[n-1] < arr[n-2]){
+    if(arr[n-1]<arr[n-2]){
         return 0;
     }
-   
-    
-    return sortedarrsy(arr,n-1);
+    return sortedarray(arr,n-1);
 }
